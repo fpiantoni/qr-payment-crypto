f2d96debd0c6ab41b95a4166baf5f950b9357c1b
fpiantoni <fran@piantoni.com.ar>
2021-11-01 12:47:56 -0300

    Base Project caye's adds

--- basic_front/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import reactDom from "react-dom";
-//import { InputBox } from './components/InputBox'
-import App from './App'
-
-//reactDom.render(<InputBox />, document.getElementById("root"))
-reactDom.render(<App />, document.getElementById("root"))
